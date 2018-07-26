c05c599b11515faa54fd467851c782a68132f5ac
Anton Shumsky <anton.shumsky@gmail.com>
2018-07-26 22:23:01 +0300

    bootstrap project

---
+++ src/main/java/com/github/shumsky/simpledocumentstore/SimpleDocumentStoreApplication.java
@@ -0,0 +1,12 @@
+package com.github.shumsky.simpledocumentstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDocumentStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDocumentStoreApplication.class, args);
+	}
+}
