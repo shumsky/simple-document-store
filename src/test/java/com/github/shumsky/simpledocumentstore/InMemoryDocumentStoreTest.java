74128ffe3e09ffc33b4df23b5cd623958a449294
Anton Shumsky <anton.shumsky@gmail.com>
2018-07-27 00:49:03 +0300

    add test for DocumentStore.findByKeyword

--- src/test/java/com/github/shumsky/simpledocumentstore/InMemoryDocumentStoreTest.java
+++ src/test/java/com/github/shumsky/simpledocumentstore/InMemoryDocumentStoreTest.java
@@ -8,11 +8,21 @@ public class InMemoryDocumentStoreTest {
 
     @Test
     public void testInsertAndFindDocument() {
-        DocumentStore store = new InMemoryDocumentStore();
+        DocumentStore store = new InMemoryDocumentStore(index);
         String document = "foo bar baz";
         String documentId = "foo bar baz";
         store.insert(documentId, document);
         String foundDocument = store.find(documentId);
+        assertEquals(document, foundDocument);
+    }
+
+    @Test
+    public void testFindDocumentByKeyword() {
+        DocumentStore store = new InMemoryDocumentStore(index);
+        String document = "foo bar";
+        String documentId = "123";
+        store.insert(documentId, document);
+        String foundDocument = store.findByKeywords("bar");
         assertEquals(document, foundDocument);
     }
 }
