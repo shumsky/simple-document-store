2e180ac20d33062f0cd5549cabaddf8ed5ed51ed
Anton Shumsky <anton.shumsky@gmail.com>
2018-07-29 20:54:48 +0300

    extract private doRequest method in DocumentStoreClient

--- document-store-cli/src/main/java/com/github/shumsky/documentstorecli/DocumentStoreHttpClient.java
+++ document-store-cli/src/main/java/com/github/shumsky/documentstorecli/DocumentStoreHttpClient.java
@@ -4,12 +4,12 @@ import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPut;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -19,6 +19,7 @@ import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.BiFunction;
 
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
@@ -29,7 +30,6 @@ public class DocumentStoreHttpClient implements DocumentStoreClient {
 
     private final String baseUri;
     private final CloseableHttpClient httpClient;
-    private final RequestConfig requestConfig;
 
     public DocumentStoreHttpClient(String host, int port) {
         this(host, port, DEFAULT_TIMEOUT_MILLIS);
@@ -45,74 +45,70 @@ public class DocumentStoreHttpClient implements DocumentStoreClient {
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
-        httpClient = HttpClients.createDefault();
-        requestConfig = RequestConfig.custom()
+
+        RequestConfig requestConfig = RequestConfig.custom()
                 .setConnectionRequestTimeout(timeoutMillis)
                 .setConnectTimeout(timeoutMillis)
                 .setSocketTimeout(timeoutMillis)
+                .build();
+
+        httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(requestConfig)
                 .build();
     }
 
     @Override
     public void put(String key, String document) {
         HttpPut httpPut = new HttpPut(baseUri + "/documents/" + key);
-        httpPut.setConfig(requestConfig);
         httpPut.setEntity(new StringEntity(document, ContentType.create("text/plain", "utf-8")));
-        try {
-            CloseableHttpResponse response = httpClient.execute(httpPut);
-            int statusCode = response.getStatusLine().getStatusCode();
+
+        doRequest(httpPut, (statusCode, responseBody) -> {
             if (statusCode != 201 && statusCode != 200) {
                 throw new DocumentStoreClientException("Bad response: " + statusCode);
             }
-        } catch (IOException e) {
-            throw new DocumentStoreClientException("Failed to communicate with DocumentStore", e);
-        }
+            return null;
+        });
     }
 
     @Override
     public Optional<String> get(String key) {
         HttpGet httpGet = new HttpGet(baseUri + "/documents/" + key);
-        httpGet.setConfig(requestConfig);
-        try {
-            CloseableHttpResponse response = httpClient.execute(httpGet);
-            int statusCode = response.getStatusLine().getStatusCode();
+
+        return doRequest(httpGet, (statusCode, responseBody) -> {
             if (statusCode == 200) {
-                String responseBody = EntityUtils.toString(response.getEntity(), "utf-8");
                 return Optional.of(responseBody);
             } else if (statusCode == 404) {
                 return Optional.empty();
             } else {
                 throw new DocumentStoreClientException("Bad response: " + statusCode);
             }
-        } catch (IOException e) {
-            throw new DocumentStoreClientException("Failed to communicate with DocumentStore", e);
-        }
+        });
     }
 
     @Override
     public Collection<String> getByTokens(Collection<String> tokens) {
         String tokensParam = tokens.stream().collect(joining(","));
         HttpGet httpGet = new HttpGet(baseUri + "/documents?tokens=" + tokensParam);
-        httpGet.setConfig(requestConfig);
-        try {
-            CloseableHttpResponse response = httpClient.execute(httpGet);
+
+        return doRequest(httpGet, (statusCode, responseBody) -> {
+            List<String> documentIds = new JSONArray(responseBody).toList().stream().map(el -> {
+                if (!(el instanceof String)) {
+                    throw new DocumentStoreClientException("Invalid response");
+                }
+                return (String) el;
+            }).collect(toList());
+
+            return documentIds;
+        });
+    }
+
+    private <T> T doRequest(HttpRequestBase request, BiFunction<Integer, String, T> responseHandler) {
+        try (CloseableHttpResponse response = httpClient.execute(request)) {
             int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode == 200) {
-                String responseBody = EntityUtils.toString(response.getEntity(), "utf-8");
-
-                List<String> documentIds = new JSONArray(responseBody).toList().stream().map(el -> {
-                    if (!(el instanceof String)) {
-                        throw new DocumentStoreClientException("Invalid response");
-                    }
-                    return (String) el;
-                }).collect(toList());
-
-                return documentIds;
-            } else {
-                throw new DocumentStoreClientException("Bad response: " + statusCode);
-            }
+            String responseBody = EntityUtils.toString(response.getEntity(), "utf-8");
+            return responseHandler.apply(statusCode, responseBody);
         } catch (IOException e) {
-            throw new DocumentStoreClientException("Failed to communicate with DocumentStore", e);
+            throw new DocumentStoreClientException("Failed to communicate with server", e);
         } catch (JSONException e) {
             throw new DocumentStoreClientException("Invalid response", e);
         }
